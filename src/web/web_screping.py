f0f2b4c59eded89b203aa45759fcb53ed3a7c39a
TrybinD <94229257+TrybinD@users.noreply.github.com>
2023-09-05 13:41:28 +0300

    fixes

--- src/web/web_screping.py
+++ src/web/web_screping.py
@@ -8,7 +8,6 @@ url = 'https://dostavka24.pro/goryachaya-liniya-ashan/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-
 data = []
 
     # Extract text from <h2> tags
@@ -27,9 +26,7 @@ for p_tag in p_tags:
 folder_path = 'C:/Users/Tseh/Desktop/Hackaton'  
 csv_file_name = 'parsed_data.csv'
 
-
 full_file_path = os.path.join(folder_path, csv_file_name)
-
 
 with open(full_file_path, 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
@@ -37,7 +34,6 @@ with open(full_file_path, 'w', newline='') as csv_file:
 
     # csv_writer.writerow(['Tag', 'Text'])
     
- 
     csv_writer.writerows(data)
 
 print(f'Data has been written to {full_file_path}')
