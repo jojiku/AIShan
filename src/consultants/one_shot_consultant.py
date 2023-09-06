24032a0196f307897f35816b3c1f450cbe1e0edb
Trubin <trybin420@gmail.com>
2023-09-06 11:15:20 +0300

    bot fix

--- src/consultants/one_shot_consultant.py
+++ src/consultants/one_shot_consultant.py
@@ -1,21 +1,22 @@
-from typing import Type
+from typing import Union, Callable, Type
 
 from .consultant_interface import ConsultantInterface
 from src.classification_models.base import TestClassificationModel
+from src.actions import tell_joke
 
 classification_model = TestClassificationModel()
 
-intent_dict = {"joke": "Шутка"}
+intent_dict = {"joke": tell_joke}
 
 
 class OneShotConsultant(ConsultantInterface):
     
-    def get_answer(self, chat: list, *args, **kwargs) -> str:
+    def get_answer(self, chat: list, *args, **kwargs) -> Union[str, Callable, Type[ConsultantInterface]]:
 
         client_text = chat[-1]["content"]
 
         intent = classification_model.define_intent(client_text)
 
-        msg = intent_dict.get(intent, "Не понял вас")
+        response = intent_dict.get(intent, "Не понял вас")
 
-        return msg
+        return response
